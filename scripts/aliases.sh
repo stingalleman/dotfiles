ba66679c686cdc6b2a34f963d3e38ee3635d702a
Sting Alleman <stingalleman@icloud.com>
2020-11-21 16:28:16 +0100

    update aliases

---
+++ scripts/aliases.sh
@@ -0,0 +1,13 @@
+# SSH servers
+alias serv='ssh vps.alleman.tech'
+
+# LS
+alias ls='gls --color=auto --group-directories-first'
+alias la='ls -A'
+alias l='ls -la'
+
+# Verbose copyin1g
+alias cp='cp -iv'
+
+# Vim
+alias vi='vim'

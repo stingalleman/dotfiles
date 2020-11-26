98a0a11632dd1dfac87a87f40ec61df04770f77a
Sting Alleman <stingalleman@icloud.com>
2020-11-26 19:10:59 +0100

    add git commit -m alias

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -1,6 +1,10 @@
 # SSH servers
 alias serv='ssh vps.alleman.tech'
 
+# Github
+alias gc='git commit'
+alias gcm='git commit -m'
+
 # LS
 alias ls='gls --color=auto --group-directories-first'
 alias la='ls -A'

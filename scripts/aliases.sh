3ad736b3b820ec1bab8b26141bc695dd264bea6e
Sting Alleman <stingalleman@icloud.com>
2020-12-13 23:35:35 +0100

    add git log alias color thingy yes

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -4,7 +4,8 @@ alias serv='ssh vps.alleman.tech'
 # Github
 alias gc='git commit'
 alias gcm='git commit -m'
-
+alias gl='git log --date=short --color --graph --pretty=format:'\''%Cgreen[%Creset%h%Cgreen]%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)%Creset'\'' --abbrev-commit'
+alias gitlog = 'gl'
 # LS
 alias ls='gls --color=auto --group-directories-first'
 alias la='ls -A'

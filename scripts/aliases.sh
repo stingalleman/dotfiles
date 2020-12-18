528fb626216ebf8544ef07046164816dcd85729c
Sting Alleman <stingalleman@icloud.com>
2020-12-18 22:57:56 +0100

    remove broken alias

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -5,7 +5,7 @@ alias serv='ssh vps.alleman.tech'
 alias gc='git commit'
 alias gcm='git commit -m'
 alias gl='git log --date=short --color --graph --pretty=format:'\''%Cgreen[%Creset%h%Cgreen]%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)%Creset'\'' --abbrev-commit'
-alias gitlog = 'gl'
+
 # LS
 alias ls='gls --color=auto --group-directories-first'
 alias la='ls -A'

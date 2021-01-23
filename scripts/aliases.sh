fff03d7c31ca3798bdf9be3cefd2b89938ced2a5
Sting Alleman <stingalleman@icloud.com>
2021-01-23 16:48:41 +0100

    new ls, gopath and go to bin

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -7,9 +7,7 @@ alias gcm='git commit -m'
 alias gl='git log --date=short --color --graph --pretty=format:'\''%Cgreen[%Creset%h%Cgreen]%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)%Creset'\'' --abbrev-commit'
 
 # LS
-alias ls='gls --color=auto --group-directories-first'
-alias la='ls -A'
-alias l='ls -la'
+alias ls='lsd'
 
 # Verbose copyin1g
 alias cp='cp -iv'

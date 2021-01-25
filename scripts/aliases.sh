5e1eb3be454bc6aaeafeeb82cce5b36f063c9d6b
Sting Alleman <stingalleman@icloud.com>
2021-01-25 02:37:10 +0100

    add lsd

--- scripts/aliases.sh
+++ scripts/aliases.sh
@@ -7,7 +7,8 @@ alias gcm='git commit -m'
 alias gl='git log --date=short --color --graph --pretty=format:'\''%Cgreen[%Creset%h%Cgreen]%Creset -%C(yellow)%d%Creset %s %Cgreen(%cd) %C(bold blue)%Creset'\'' --abbrev-commit'
 
 # LS
-alias ls='gls --color=auto --group-directories-first'
+alias ls='lsd'
+alias lss='gls --color=auto --group-directories-first'
 alias la='ls -A'
 alias l='ls -la'
 

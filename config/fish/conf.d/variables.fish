50229f8e4febd3e02467395c9f5f6cba624064f2
Sting Alleman <stingalleman@icloud.com>
2021-04-15 00:01:37 +0200

    vars

--- config/fish/conf.d/variables.fish
+++ config/fish/conf.d/variables.fish
@@ -1,6 +1,8 @@
-set -Ux EDITOR "micro"
-set -Ux GOPATH "$HOME/.go"
+export EDITOR="micro"
+export GOPATH="$HOME/.go"
 
-set -Ux CONTAINERS_PATH "$HOME/containers"
+export CONTAINERS_PATH="$HOME/containers"
 
-set -Ux HOMEBREW_NO_AUTO_UPDATE 1
+export HOMEBREW_NO_AUTO_UPDATE="1"
+
+export PATH="$PATH $GOPATH/bin"

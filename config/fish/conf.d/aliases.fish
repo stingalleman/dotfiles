a1a241e0ffd8b1193eafb94134821f99ca99d77c
Sting Alleman <stingalleman@icloud.com>
2022-04-01 03:01:29 +0200

    things

--- config/fish/conf.d/aliases.fish
+++ config/fish/conf.d/aliases.fish
@@ -32,9 +32,6 @@ alias cp='cp -iv'
 # node
 alias pscripts='jq .scripts package.json | bat -l json'
 
-# TC
-alias tc-compose='docker-compose --env-file /home/server/projects/tribecamp/tribecamp/.env  -f /home/server/projects/tribecamp/tribecamp/docker/db/docker-compose.yml -f /home/server/projects/tribecamp/tribecamp/docker/development/docker-compose.yml -f /home/server/projects/tribecamp/tribecamp/docker/kong/docker-compose.yml -p tc '
-
 # golang
 alias grun='go run .'
 alias sgrun='sudo go run .'
@@ -44,3 +41,7 @@ alias jesse='curl -s https://jesse.frij.link/ | bash | cowsay'
 
 # unlock gpg
 alias unlock='echo | gpg --sign'
+
+# DNS
+alias flushdns='sudo dscacheutil -flushcache;sudo killall -HUP mDNSResponder'
+
